fd7e54cee2a66af303844fed002c55d4d1e8c323
DistantBlue <exotic.xD@gmail.com>
2019-08-02 14:02:28 +0300

    ExerciseListFragment, ExerciseViewModel, exercise_list_fragment_layout files added

---
+++ app/src/main/java/com/thedistantblue/triaryapp/mainscreen/TrainingFlow/ExerciseListFragment.java
@@ -0,0 +1,6 @@
+package com.thedistantblue.triaryapp.mainscreen.TrainingFlow;
+
+import androidx.fragment.app.Fragment;
+
+public class ExerciseListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/thedistantblue/triaryapp/viewmodels/ExerciseViewModel.java
@@ -0,0 +1,15 @@
+package com.thedistantblue.triaryapp.viewmodels;
+
+import com.thedistantblue.triaryapp.entities.Exercise;
+
+public class ExerciseViewModel {
+    private Exercise exercise;
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+}
