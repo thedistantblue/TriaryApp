bd73deffc124d88dc646cc786bb08362a49c1ef3
DistantBlue <exotic.xD@gmail.com>
2019-08-02 14:21:26 +0300

    ExerciseListFragment in progress

--- app/src/main/java/com/thedistantblue/triaryapp/mainscreen/TrainingFlow/ExerciseListFragment.java
+++ app/src/main/java/com/thedistantblue/triaryapp/mainscreen/TrainingFlow/ExerciseListFragment.java
@@ -1,6 +1,30 @@
 package com.thedistantblue.triaryapp.mainscreen.TrainingFlow;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.thedistantblue.triaryapp.R;
+import com.thedistantblue.triaryapp.databinding.ExerciseListFragmentLayoutBinding;
+
 public class ExerciseListFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        ExerciseListFragmentLayoutBinding binding =
+                DataBindingUtil.inflate(inflater, R.layout.exercise_list_fragment_layout, parent, false);
+
+
+
+        return binding.getRoot();
+    }
 }
