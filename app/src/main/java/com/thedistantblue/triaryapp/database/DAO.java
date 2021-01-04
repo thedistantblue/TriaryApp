0d73cf362c318d3844e6611a6a7fa0cf4c1970a5
DistantBlue <exotic.xD@gmail.com>
2021-01-04 16:58:44 +0500

    Removes comments in DAO interface

--- app/src/main/java/com/thedistantblue/triaryapp/database/DAO.java
+++ app/src/main/java/com/thedistantblue/triaryapp/database/DAO.java
@@ -33,32 +33,16 @@ public interface DAO {
 
     User getUser(User user);
 
-    // ВОЗМОЖНО НЕПРАВИЛЬНО!
-    // Тренировка представляет из себя список упражнений, поэтому
-    // при получении самой тренировки, вы должны и получить ассоциированный с ней
-    // список упражнений.
-    // Сделал так, как должно быть, но возможны ошибки. Перевроверить.
-    // UPD: По-идее, этот метод вообще не нужен, т.к. все необходимое делается в
-    // получении списка, а сама тренировка берется при помощи курсор враппера.
     Training getTraining(Training training);
 
-    // ВНИМАНИЕ!
-    // Пользователь сейчас ищется по пустому UUID, что захардкодено.
-    // Как задать пустой UUID: UUID.fromString("");
     List<Training> getTrainingsList(User user);
 
-    // ВОЗМОЖНО НЕПРАВИЛЬНО!
-    // Получая конкретное упражнение, мы должны также взять список сетов из
-    // соответствующей таблицы.
     Exercise getExercise(Exercise exercise);
 
     List<Exercise> getExercisesList(Dates dates);
 
     Set getSet(Exercise exercise, int number);
 
-    // ВНИМАНИЕ!
-    // Не надо ли получать и задавать каждый сет отдельно?
-    // Получение написано выше.
     List<Set> getSetsList(Exercise exercise);
 
     List<Dates> getDates(Training training);
@@ -67,25 +51,20 @@ public interface DAO {
 
     List<Running> getRunningList(User user);
 
-    // Обновляем тренировку
     void updateTraining(Training training);
 
-    // Обновляем упражнение
     void updateExercise(Exercise exercise);
 
-    // Обновляем сет
     void updateSet(Set set);
 
     void updateRunning(Running running);
 
-    // Удаляем пользователя
     void deleteUser(User user);
 
     void deleteTraining(Training training);
 
     void deleteDate(Dates dates);
 
-    // Удаляем упражнение из БД и ассоциированные с ним сеты
     void deleteExercise(Exercise exercise);
 
     void deleteSet(Set set);
