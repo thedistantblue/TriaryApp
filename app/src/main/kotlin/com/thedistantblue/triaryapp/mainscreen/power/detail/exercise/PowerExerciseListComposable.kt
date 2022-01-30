e2930b53b138926a2b85f3ea1716d0a589eee52a
Jack <none>
2022-01-30 05:12:09 +0300

    Adds some attempts to navigate from composable
    refs #18

--- app/src/main/kotlin/com/thedistantblue/triaryapp/mainscreen/power/detail/exercise/PowerExerciseListComposable.kt
+++ app/src/main/kotlin/com/thedistantblue/triaryapp/mainscreen/power/detail/exercise/PowerExerciseListComposable.kt
@@ -1,41 +1,83 @@
 package com.thedistantblue.triaryapp.mainscreen.power.detail.exercise
 
+import android.view.View
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.rxjava3.subscribeAsState
 import androidx.compose.ui.unit.dp
+import com.thedistantblue.triaryapp.database.room.dao.ExerciseDetailsDao
 import com.thedistantblue.triaryapp.entities.composite.details.ExerciseDetails
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
-@Preview
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.thedistantblue.triaryapp.mainscreen.power.detail.PowerTrainingDetailFragmentDirections
+
 @Composable
-fun PowerExerciseListScreen() {
+fun PowerExerciseListScreen(
+    trainingId: String,
+    exerciseDao: ExerciseDetailsDao,
+    lifecycleOwner: LifecycleOwner,
+    navFunction: () -> Unit
+) {
     Scaffold(
         content = {
-            ExerciseListWithFab()
+            ExerciseList(trainingId, exerciseDao, lifecycleOwner)
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text(text = "Create Power Exercise") },
+                onClick = navFunction
+            )
         }
     )
 }
 
 @Composable
-fun ExerciseListWithFab() {
-    val exercises = listOf<ExerciseDetails>()
-    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
-        items(exercises) { data ->
-            ExerciseListItem(exerciseDetails = data)
+private fun ExerciseList(trainingId: String, exerciseDao: ExerciseDetailsDao, lifecycleOwner: LifecycleOwner) {
+    val exercisesSingle = exerciseDao.findAllByTrainingId(trainingId)
+    val exercisesState by exercisesSingle.subscribeAsState(initial = mutableListOf())
+
+    val observer = LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_RESUME) {
+            exerciseDao.findAllByTrainingId(trainingId).subscribe { exercises ->
+                exercisesState.clear()
+                exercisesState.addAll(exercises)
+            }
         }
     }
-    ExtendedFloatingActionButton(
-        text = { Text(text  = "Some EFAB TEXT") },
-        onClick = { /*TODO*/ }
-    )
+    lifecycleOwner.lifecycle.addObserver(observer)
+
+    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+        items(exercisesState!!) { item ->
+            ExerciseListItem(exerciseDetails = item)
+        }
+    }
 }
 
 @Composable
-fun ExerciseListItem(exerciseDetails: ExerciseDetails) {
-
+private fun ExerciseListItem(exerciseDetails: ExerciseDetails) {
+    Card(
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "asdasd")
+        }
+    }
 }
--- app/src/main/kotlin/com/thedistantblue/triaryapp/mainscreen/power/detail/exercise/PowerExerciseListFragmentCompose.kt
+++ app/src/main/kotlin/com/thedistantblue/triaryapp/mainscreen/power/detail/exercise/PowerExerciseListFragmentCompose.kt
@@ -6,13 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import com.thedistantblue.triaryapp.R
+import com.thedistantblue.triaryapp.database.room.dao.ExerciseDetailsDao
+import com.thedistantblue.triaryapp.database.room.database.RoomDataBaseProvider
+import com.thedistantblue.triaryapp.entities.base.Training
+import com.thedistantblue.triaryapp.entities.composite.details.TrainingDetails
 
 class PowerExerciseListFragmentCompose: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    lateinit var exerciseDetailsDao: ExerciseDetailsDao
+    lateinit var trainingDetails: TrainingDetails
+    lateinit var lifecycleOwner: LifecycleOwner
+    lateinit var composeView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        exerciseDetailsDao = RoomDataBaseProvider.getDatabaseWithProxy(requireActivity()).exerciseDetailsDao()
+        trainingDetails = PowerExerciseListFragmentComposeArgs.fromBundle(requireArguments()).trainingDetails
+        lifecycleOwner = this
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        //val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.tab_power_nav_host) as NavHostFragment?
+
         return ComposeView(requireActivity()).apply {
             setContent {
-                PowerExerciseListScreen()
+                PowerExerciseListScreen("trainingDetails.training.trainingId",
+                                        exerciseDetailsDao,
+                                        lifecycleOwner) {
+                    doNavigate()
+                }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        composeView = view
+    }
+
+    private fun doNavigate() {
+        val directions = PowerExerciseListFragmentComposeDirections.actionPowerExerciseListFragmentToPowerExerciseCreationFragment(Training(1))
+        Navigation.findNavController(composeView).navigate(directions)
     }
 }
